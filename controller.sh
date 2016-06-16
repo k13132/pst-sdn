275909385bc7392418f414f61be7561cd5dccb52
Tomáš Kukrál <kukratom@fit.cvut.cz>
2016-06-16 10:34:59 +0200

    publish 6633 port when running in docker

--- controller.sh
+++ controller.sh
@@ -9,7 +9,7 @@ fi
 if [ "$2" != "docker" ]; then
 	cmd="ryu-manager \"$file\""
 else
-	cmd="docker run --rm -ti --volume $PWD:/usr/share/app/files/:ro tomkukral/ryu ryu-manager files/$file"
+	cmd="docker run --rm -ti -p 6633:6633 --volume $PWD:/usr/share/app/files/:ro tomkukral/ryu ryu-manager files/$file"
 fi
 
 echo "$cmd"
